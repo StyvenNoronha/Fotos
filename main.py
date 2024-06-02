97e95c03b5617a49fb2411f3c8582b4a39fd10af
styven <styvenn16@gmail.com>
2024-06-02 08:31:23 -0300

    renderizar uma pagina com html

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def homepage():
-    return "meu primeiro site com flask"
+    return render_template("homepage.html")
+
+
+
 
 
 if __name__=="__main__":
