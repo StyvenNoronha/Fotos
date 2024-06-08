5a0a20cfe767911a97113249785852a18fa87b64
styven <styvenn16@gmail.com>
2024-06-08 19:12:26 -0300

    criação do formulario no html

--- core/form.py
+++ core/form.py
@@ -2,13 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from core.models import Usuario
-class FormLogin():
+class FormLogin(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     senha = PasswordField("Senha", validators=[DataRequired()])
     botao_confirmacao = SubmitField("Fazer Login")
 
 
-class FormCriarConta():
+class FormCriarConta(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     username = StringField("Nome de Usuario", validators=[DataRequired()])
     senha = PasswordField("Senha", validators=[DataRequired(), Length(6,20)])
--- core/routes.py
+++ core/routes.py
@@ -1,14 +1,20 @@
 from flask import render_template, url_for
 from core import app
 from flask_login import login_required
-@app.route("/")
+from core.form import FormLogin, FormCriarConta
+@app.route("/", methods=["GET","POST"])
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", form = formlogin)
 
 
+@app.route("/criarconta", methods=["GET","POST"])
+def criarconta(): 
+    formcriarconta = FormCriarConta()
+    return render_template("criarconta.html", form = formcriarconta)
 
 
-@app.route("/perfil/<usuario>")
-@login_required
+@app.route("/perfil/<usuario>", methods=["GET","POST"])
+#@login_required
 def perfil(usuario):
     return render_template("perfil.html",usuario=usuario)
